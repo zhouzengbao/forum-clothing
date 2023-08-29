6d118b16bb0b0d728aac2cee071d171151d5d6e3
zb <zhouzb27@gmail.com>
2023-08-29 13:29:16 +0800

    添加swagger

--- src/main/java/com/forum/clothing/dto/InItUserInfoDto.java
+++ src/main/java/com/forum/clothing/dto/InItUserInfoDto.java
@@ -1,5 +1,7 @@
 package com.forum.clothing.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +13,7 @@ import java.util.List;
 /**
  * 用户注册信息
  */
+@ApiModel(value = "Blog", description = "用户注册传入对象")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -19,38 +22,40 @@ public class InItUserInfoDto {
     /**
      * 姓名
      */
+    @ApiModelProperty(value = "姓名")
     @NotNull(message = "姓名不能为空")
     private String userName;
     /**
      * 身份证号
      */
+    @ApiModelProperty(value = "身份证号")
     @NotNull(message = "身份证号不能为空")
     private String idNum;
     /**
      * 用户类型
      */
+    @ApiModelProperty(value = "用户类型 1供货商2经销商")
     @NotNull(message = "请选择用户类型")
     private Integer userType;
     /**
      * 手机号
      */
+    @ApiModelProperty(value = "手机号 由小程序注册获得")
     @NotNull(message = "手机号不能为空")
     private String telephone;
     /**
-     * 验证码
-     */
-    @NotNull(message = "验证码不能为空")
-    private String code;
-    /**
      * 邀请码
      */
+    @ApiModelProperty(value = "邀请码")
     private String inviteCode;
     /**
      * 图片
      */
     @Size(min = 3, max = 3, message = "请上传3张认证图片")
+    @ApiModelProperty(value = "认证图片")
     private List<String> sitePic;
 
+    @ApiModelProperty(value = "用户ID，使用code换取的用户信息中的ID")
     @NotNull(message = "参数错误!")
     private String userId;
 }
--- src/main/java/com/forum/clothing/model/AppUser.java
+++ src/main/java/com/forum/clothing/model/AppUser.java
@@ -3,6 +3,8 @@ package com.forum.clothing.model;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,75 +18,92 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 @TableName("app_user")
+@Api("用户基础类")
 public class AppUser {
 
     @TableId(value = "id", type = IdType.AUTO)
+    @ApiModelProperty(value = "用户主键")
     private Integer id;
 
     /**
      * 昵称
      */
+    @ApiModelProperty(value = "用户昵称")
     private String nickName;
 
+    @ApiModelProperty(value = "姓名")
     private String userName;
 
     /**
      * 手机号
      */
+    @ApiModelProperty(value = "手机号")
     private String telephone;
 
     /**
      * 用户号
      */
+    @ApiModelProperty(value = "用户号")
     private String userCode;
 
     /**
      * 用户类型：1供货商2经销商
      */
+    @ApiModelProperty(value = "用户类型1供货商 2经销商")
     private Integer userType;
 
     /**
      * 身份证号
      */
+    @ApiModelProperty(value = "身份证号")
     private String idNum;
 
     /**
      * 0未认证 1已认证
      */
+    @ApiModelProperty(value = "认证状态0未认证 1已认证")
     private Integer auth;
 
     /**
      * 认证金额
      */
+    @ApiModelProperty(value = "认证金额")
     private Long authAmount;
 
     /**
      * 邀请人ID
      */
+    @ApiModelProperty(value = "邀请人code")
     private String inviteUserId;
 
     /**
      * openId
      */
+    @ApiModelProperty(value = "openId")
     private String openId;
 
     /**
      *
      */
+    @ApiModelProperty(value = "审核过期时间")
     private Long expireTime;
 
+    @ApiModelProperty(value = "创建时间")
     private Long createTime;
 
+    @ApiModelProperty(value = "最后更新时间")
     private Long updateTime;
 
     /**
      * 头像
      */
+    @ApiModelProperty(value = "头像")
     private String avatarUrl;
 
     /**
      * 认证图片 逗号割
      */
+    @ApiModelProperty(value = "认证图片")
     private String sitePic;
 
 }
